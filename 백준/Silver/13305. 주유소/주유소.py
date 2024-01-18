9deaca3de98536b95d83ac06c36251ddeb927ad7
JiHyeong23 <116024567+JiHyeong23@users.noreply.github.com>
2024-01-18 13:21:20 +0900

    [Silver III] Title: 주유소, Time: 104 ms, Memory: 42148 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -6,20 +6,25 @@ n = int(input()) #도시의 갯수
 load = list(map(int, input().split())) #도시 간 거리
 cost = deque(map(int, input().split())) #도시 별 리터당 주유비
 cost.pop()
-
 total = 0
-if cost[0] == min(cost):
-    print(cost[0] * sum(load))
+i = 0
+while cost:
+    min_cost = min(cost) #남은 도시중 가장 싼 가격
+    city = cost.popleft() #도착한 도시의 주유
     
-else:
-    total += cost.popleft() * load[0] #일단사
+    if city == min_cost: #제일 낮은 가격이면
+        total += city * sum(load[i:]) #남은만큼 다 주유
+        break
+    
+    elif city < cost[0]: #뒷 도시보다 싸면
+        total += city * load[i]
+        while cost and city < cost[0]: #계속 비교
+            i += 1
+            total += city * load[i] #다음 도시까지 주유
+            cost.popleft()
+    else:
+        total += city * load[i]
 
-    for i in range(1, len(cost)):
-        min_cost = min(cost)
-        city = cost.popleft()
-        if city == min_cost:
-            total += city * sum(load[i:])
-        else:
-            total += city * load[i]
+    i += 1
 
-    print(total)
+print(total)
