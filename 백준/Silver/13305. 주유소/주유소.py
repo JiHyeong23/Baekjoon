2e1c9b6fc5ab33a074fb221d2d00fd70ec485f06
JiHyeong23 <116024567+JiHyeong23@users.noreply.github.com>
2024-01-18 13:30:45 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 46228 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,30 +1,15 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-n = int(input()) #도시의 갯수
-load = list(map(int, input().split())) #도시 간 거리
-cost = deque(map(int, input().split())) #도시 별 리터당 주유비
-cost.pop()
-total = 0
-i = 0
-while cost:
-    min_cost = min(cost) #남은 도시중 가장 싼 가격
-    city = cost.popleft() #도착한 도시의 주유
-    
-    if city == min_cost: #제일 낮은 가격이면
-        total += city * sum(load[i:]) #남은만큼 다 주유
-        break
-    
-    elif city < cost[0]: #뒷 도시보다 싸면
-        total += city * load[i]
-        while cost and city < cost[0]: #계속 비교
-            i += 1
-            total += city * load[i] #다음 도시까지 주유
-            cost.popleft()
-    else:
-        total += city * load[i]
+n = int(input()) 
+road = list(map(int, input().split())) 
+cost = list(map(int, input().split())) 
 
-    i += 1
+total = 0
+min_cost = 1000000001
+for i in range(n-1):
+    if cost[i] < min_cost:
+        min_cost = cost[i]
+    total += road[i] * min_cost
 
 print(total)
