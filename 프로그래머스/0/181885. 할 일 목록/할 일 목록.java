f8e773237d8b5b84117bd86121b27f2113c26b44
JiHyeong23 <116024567+JiHyeong23@users.noreply.github.com>
2024-06-19 18:12:44 +0900

    [level 0] Title: 할 일 목록, Time: 0.02 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        int len = 0;
+        for (boolean b : finished) {
+            if (!b) {
+                len++;
+            }
+        }
+        String[] answer = new String[len];
+        int idx = 0;
+        for(int i = 0; i < finished.length; i++) {
+            if (!finished[i]) {
+                answer[idx] = todo_list[i];
+                idx++;
+            }
+        }
+        return answer;
+    }
+}
