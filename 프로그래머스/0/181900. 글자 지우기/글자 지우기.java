7d666a3b5a0bb795ab3c3ccbb54e9b7afee88afa
JiHyeong23 <116024567+JiHyeong23@users.noreply.github.com>
2024-07-03 11:38:59 +0900

    [level 0] Title: 글자 지우기, Time: 12.63 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        
+        List<Integer> list = 
+            Arrays.stream(indices).boxed()
+                .collect(Collectors.toList());
+        
+        for(int i = 0; i < my_string.length(); i++) {
+            if(list.contains(i)) {
+                continue;
+            } else {
+                answer += my_string.charAt(i);
+            }
+        }
+        
+        return answer;
+    }
+}
